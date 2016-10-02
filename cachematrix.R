69ebbc2c2be20aa5bebe98571148ea004ef23626
Carlo Navarro <carmelofranconavarro@gmail.com>
2016-10-02 23:18:39 +0800

    introduced changes for Programing Assignment 2 - Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Programming Assignment 2 for R Programming. 
+## makeCacheMatrix: Creates a Special Matrix: List to contain a function 
+##      to set/get the value of the matrix, 
+##      set/get the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
+        
+        
 }
 
 
-## Write a short comment describing this function
+## cacheSolve: Solves the inverse of the matrix using solve(). 
+## Unless the inverse has been cached already, then the function fetches the cache
+## Assumes the matrix is singular
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
+        
 }
